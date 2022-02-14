26c29c9b2f6094c83e38d30bb7982025db2b9b94
emre <emrebaglayici1@gmail.com>
2022-02-14 12:55:35 +0300

    Spring Data Jpa used

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student emre=new Student(
+                    "Emre","Bağlayici","emrebaglayici@gmail.com",26
+            );
+            studentRepository.save(emre);
+        };
     }
 
 }
