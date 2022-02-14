79786557d457bb2ca63123eab492f0a9ba373b7f
emre <emrebaglayici1@gmail.com>
2022-02-14 13:40:48 +0300

    Basics are done

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,6 +4,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Application {
@@ -21,5 +25,6 @@ public class Application {
             studentRepository.save(emre);
         };
     }
+
 
 }
